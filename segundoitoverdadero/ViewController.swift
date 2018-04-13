0665580e4164fda72d15600e673bf3ed30bf3622
javier <javier.rodriguez2@live.u-tad.com>
2018-04-13 17:06:22 +0200

    primer comit

---
+++ segundoitoverdadero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  segundoitoverdadero
+//
+//  Created by JAVIER SÁNCHEZ RODRIGUEZ on 13/4/18.
+//  Copyright © 2018 JAVIER SÁNCHEZ RODRIGUEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
