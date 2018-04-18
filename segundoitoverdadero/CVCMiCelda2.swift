d31d414aad6af35e0ddca64a36ae4326f598dc5e
javier <javier.rodriguez2@live.u-tad.com>
2018-04-18 21:54:36 +0200

    atascado

---
+++ segundoitoverdadero/CVCMiCelda2.swift
@@ -0,0 +1,13 @@
+//
+//  CVCMiCelda2.swift
+//  segundoitoverdadero
+//
+//  Created by JAVIER SÁNCHEZ RODRIGUEZ on 18/4/18.
+//  Copyright © 2018 JAVIER SÁNCHEZ RODRIGUEZ. All rights reserved.
+//
+
+import UIKit
+
+class CVCMiCelda2: UICollectionViewCell {
+    
+}
---
+++ segundoitoverdadero/DataHolder.swift
@@ -0,0 +1,13 @@
+//
+//  DataHolder.swift
+//  segundoitoverdadero
+//
+//  Created by JAVIER SÁNCHEZ RODRIGUEZ on 18/4/18.
+//  Copyright © 2018 JAVIER SÁNCHEZ RODRIGUEZ. All rights reserved.
+//
+
+import UIKit
+
+class DataHolder: NSObject {
+
+}
