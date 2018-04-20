e8b0842930df8f2d38380b44482a177dc257bc6a
javier <javier.rodriguez2@live.u-tad.com>
2018-04-20 21:50:21 +0200

    realice el mapa y estoy comenzando firebase que espero terminarlo el lunes.

---
+++ segundoitoverdadero/LocationAdmin.swift
@@ -0,0 +1,13 @@
+//
+//  LocationAdmin.swift
+//  segundoitoverdadero
+//
+//  Created by JAVIER SÁNCHEZ RODRIGUEZ on 20/4/18.
+//  Copyright © 2018 JAVIER SÁNCHEZ RODRIGUEZ. All rights reserved.
+//
+
+import UIKit
+
+class LocationAdmin: NSObject {
+
+}
