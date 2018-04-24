8c5ecd09cf4a046b3d96a9cc823f2a29a985fe6f
javier <javier.rodriguez2@live.u-tad.com>
2018-04-24 15:23:16 +0200

    Recuerda v2

---
+++ Pods/Target Support Files/Pods-segundoitoverdadero/Pods-segundoitoverdadero-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_segundoitoverdadero : NSObject
+@end
+@implementation PodsDummy_Pods_segundoitoverdadero
+@end
---
+++ build/Pods.build/Debug-iphoneos/Pods-segundoitoverdadero.build/DerivedSources/Pods_segundoitoverdadero_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_segundoitoverdaderoVersionString[];
+ extern const double Pods_segundoitoverdaderoVersionNumber;
+
+ const unsigned char Pods_segundoitoverdaderoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_segundoitoverdadero  PROJECT:Pods-1" "\n";
+ const double Pods_segundoitoverdaderoVersionNumber __attribute__ ((used)) = (double)1.;
