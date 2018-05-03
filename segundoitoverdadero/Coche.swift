890b788c1b3f7d4cdc5efbc0b7e2567d472f3e43
javier <javier.rodriguez2@live.u-tad.com>
2018-05-03 21:46:52 +0200

    Meto la clase coche!!

---
+++ segundoitoverdadero/Coche.swift
@@ -0,0 +1,13 @@
+//
+//  Coche.swift
+//  segundoitoverdadero
+//
+//  Created by JAVIER SÁNCHEZ RODRIGUEZ on 3/5/18.
+//  Copyright © 2018 JAVIER SÁNCHEZ RODRIGUEZ. All rights reserved.
+//
+
+import UIKit
+
+class Coche: NSObject {
+
+}
