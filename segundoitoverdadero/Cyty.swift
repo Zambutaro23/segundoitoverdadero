469342d15c468cb010e2fcc50b909f026276513d
javier <javier.rodriguez2@live.u-tad.com>
2018-05-10 19:34:05 +0200

    video colecciones Parte2 comenzada

---
+++ segundoitoverdadero/Cyty.swift
@@ -0,0 +1,13 @@
+//
+//  Cyty.swift
+//  segundoitoverdadero
+//
+//  Created by JAVIER SÁNCHEZ RODRIGUEZ on 10/5/18.
+//  Copyright © 2018 JAVIER SÁNCHEZ RODRIGUEZ. All rights reserved.
+//
+
+import UIKit
+
+class Cyty: NSObject {
+
+}
